fc91a1a9dfba7309799617976d32d5d474e18274
akagi <phuong@e-technostar.com>
2018-07-10 20:29:34 +0900

    Add Operation.

--- Source/Math/operation.h
+++ Source/Math/operation.h
@@ -1,1 +1,208 @@
 #pragma once
+#include <iostream>
+#include <unordered_map>
+#include <typeinfo>
+
+namespace op
+{
+	// Singleton operation factory
+	template<class OPERATION>
+	class factory
+	{
+	private:
+		std::unordered_map<std::string, OPERATION*> map_;
+
+		// Constructor
+		factory();
+
+		// Convert a hex to string
+		static std::string to_hex(size_t i);
+
+		// Type info
+		template<typename T>
+		struct hash_by_type 
+		{ 
+			std::string operator()() 
+			{
+				return to_hex(typeid(T).hash_code());
+			}
+		};
+		template<typename T>
+		struct hash_by_type<std::reference_wrapper<T>>
+		{
+			std::string operator()()
+			{
+				return "R" + hash_by_type<T>()();
+			}
+		};
+		template<typename T>
+		struct hash_by_type<std::vector<T>>
+		{
+			std::string operator()()
+			{
+				return "V" + hash_by_type<T>()();
+			}
+		};
+		template<typename T>
+		struct hash_by_type<const std::vector<T>>
+		{
+			std::string operator()()
+			{
+				return "V" + hash_by_type<T>()();
+			}
+		};
+		template<typename T>
+		std::string hash_by_variable(T& t)
+		{
+			return to_hex(typeid(t).hash_code());
+		}
+		template<typename T>
+		std::string hash_by_variable(std::reference_wrapper<T>& t)
+		{
+			return "R" + hash_by_variable(t.get());
+		}
+		template<typename T>
+		std::string hash_by_variable(const std::reference_wrapper<T>& t)
+		{
+			return "R" + hash_by_variable(t.get());
+		}
+		template<typename T>
+		std::string hash_by_variable(std::vector<T>& v)
+		{
+			if (!std::is_fundamental<T>::value) {
+				if (v.size() > 0) {
+					std::string h = hash_by_variable(v.at(0));
+					if (std::all_of(v.begin() + 1, v.end(), [&](T& t) { return h == hash_by_variable(t); }))
+						return "V" + h;
+				}
+			}
+			return "V" + hash_by_type<T>()();
+		}
+		template<typename T>
+		std::string hash_by_variable(const std::vector<T>& v)
+		{
+			if (!std::is_fundamental<T>::value) {
+				if (v.size() > 0) {
+					std::string h = hash_by_variable(v.at(0));
+					if (std::all_of(v.begin() + 1, v.end(), [&](const T& t) { return h == hash_by_variable(t); }))
+						return "V" + h;
+				}
+			}
+			return "V" + hash_by_type<T>()();
+		}
+
+	public:
+		// Register the operation into the map_
+		template<typename... Ts>
+		void reg(OPERATION* A)
+		{
+			std::string hash;
+			for (auto h : { hash_by_type<Ts>()()... }) hash.append(h);
+			map_.insert(make_pair(hash, A));
+		}
+
+		// Return an operation by arguments' types
+		template<class... Ts>
+		OPERATION* operation(Ts&... ts)
+		{
+			std::string hash;
+			for (auto h : { hash_by_variable(ts)... }) hash.append(h);
+			auto it = map_.find(hash);
+			if (it == map_.end()) return 0;
+			else return it->second;
+		}
+
+		static factory<OPERATION>& get()
+		{
+			static factory<OPERATION> instance;
+			return instance;
+		}
+	};
+}
+
+namespace op
+{
+	template <typename R, class Name, typename... OArgs>
+	class operation
+	{
+	public:
+		// Destructor
+		virtual ~operation();
+
+		virtual R operator()(OArgs&...) = 0;
+
+		static R perform(OArgs&...args);		
+	};
+}
+
+namespace op
+{
+	template <typename... OArgs>
+	class type_convert {
+		template<bool...> struct bool_pack;
+		template<bool... b>
+		using all_true = std::is_same<bool_pack<true, b...>, bool_pack<b..., true>>;
+
+		template<typename T, typename U> 
+		struct is_ok
+		{
+			static constexpr bool value = std::is_convertible<T&, U&>::value; 
+		};
+		template<typename T, typename U>
+		struct is_ok<T*, U*>
+		{
+			static constexpr bool value = is_ok<T&, U&>::value;
+		};
+		template<typename T, typename U>
+		struct is_ok< std::vector<T>, std::vector<U> >
+		{
+			static constexpr bool value = is_ok<T, U>::value;
+		};
+		template<typename T, typename U>
+		struct is_ok< std::vector<T> const, std::vector<U> const >
+		{
+			static constexpr bool value = is_ok<T, U>::value;
+		};
+		template<typename T, typename U>
+		struct is_ok< std::reference_wrapper<T>, std::reference_wrapper<U> >
+		{
+			static constexpr bool value = is_ok<T, U>::value;
+		};
+		template<typename T, typename U>
+		struct is_ok< std::reference_wrapper<T> const, std::reference_wrapper<U> const >
+		{
+			static constexpr bool value = is_ok<T, U>::value;
+		};
+	public:
+		template<typename... IArgs>
+		static bool valid() { return all_true<is_ok<IArgs, OArgs>::value...>::value; }
+	};
+}
+
+
+namespace op
+{
+	template <typename O, typename... IArgs>
+	class implementation : public O {};
+
+	template <typename R, class Name, typename... OArgs, template<class, typename...> class O, typename... IArgs>
+	class implementation<O<R, Name, OArgs...>, IArgs...> : public O<R, Name, OArgs...>
+	{
+	private:
+		static implementation<O<R, Name, OArgs...>, IArgs...> _instance;
+		implementation()
+		{
+			if (type_convert_t<OArgs...>::template valid<IArgs...>())
+			{
+				factory_t<O<R, Name, OArgs...>>::get().template reg<IArgs...>(this);
+			}
+			else
+			{
+				throw std::runtime_error(util::str("Invalid operation registration of : ", util::demangle(typeid(this).name())));
+			}
+		}
+	public:
+		static implementation<O<R, Name, OArgs...>, IArgs...>& get() { return _instance; };
+		virtual R operator()(OArgs&...);
+	};
+}
--- Source/MathTest/Main.cpp
+++ Source/MathTest/Main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <unordered_map>
+#include <vector>
+#include <cstdarg>
 #include "mpi.h"
 
 void TestMPI(int argc, char* argv[])
@@ -57,10 +62,141 @@ void TestTemplateFunctions()
 }
 #pragma endregion
 
+#pragma region TestHex2String
+std::string to_hex(size_t i)
+{
+	std::ostringstream oss;
+	oss << std::setfill('0') << std::setw(16) << std::hex << i;
+	return oss.str();
+}
+void TestHex2String()
+{
+	std::string str = to_hex(16);
+	std::cout << str << std::endl;
+}
+#pragma endregion
+
+#pragma region TestTypeInfo
+template<typename T>
+struct hash_by_type
+{
+	std::string operator()()
+	{
+		return to_hex(typeid(T).hash_code());
+	}
+};
+template<typename T>
+std::string hash_by_variable(T& t)
+{
+	return to_hex(typeid(t).hash_code());
+}
+
+struct A{};
+struct B : public A {};
+struct C : public A {};
+
+using TypeInfoRef = std::reference_wrapper<const std::type_info>;
+struct Hasher {
+	std::size_t operator()(TypeInfoRef code) const
+	{
+		return code.get().hash_code();
+	}
+};
+struct EqualTo {
+	bool operator()(TypeInfoRef lhs, TypeInfoRef rhs) const
+	{
+		return lhs.get() == rhs.get();
+	}
+};
+void TestTypeInfo()
+{	
+	std::unordered_map<TypeInfoRef, std::string, Hasher, EqualTo> typenames;
+	typenames[typeid(int)] = "int";
+	typenames[typeid(double)] = "double";
+	typenames[typeid(float)] = "float";
+	typenames[typeid(A)] = "A";
+	typenames[typeid(B)] = "B";
+	typenames[typeid(C)] = "C";
+
+	int i;
+	std::cout << typeid(i).name() << std::endl;
+	std::cout << "Type of i is " << typenames[typeid(i)] << std::endl;
+	std::cout << hash_by_type<int>()() << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+
+	double d;
+	std::cout << typeid(d).name() << std::endl;
+	std::cout << "Type of d is " << typenames[typeid(d)] << std::endl;
+	std::cout << hash_by_type<double>()() << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+
+	float f;
+	std::cout << typeid(f).name() << std::endl;
+	std::cout << "Type of f is " << typenames[typeid(f)] << std::endl;
+	std::cout << hash_by_type<float>()() << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+
+	A a;
+	std::cout << typeid(a).name() << std::endl;
+	std::cout << "Type of a is " << typenames[typeid(a)] << std::endl;
+	std::cout << hash_by_variable<A>(a) << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+
+	A* b = new B();
+	std::cout << typeid(*b).name() << std::endl;
+	std::cout << "Type of b is " << typenames[typeid(*b)] << std::endl;
+	std::cout << hash_by_variable<A>(*b) << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+
+	A* c = new C();
+	std::cout << typeid(*c).name() << std::endl;
+	std::cout << "Type of c is " << typenames[typeid(*c)] << std::endl;
+	std::cout << hash_by_variable<A>(*c) << std::endl;
+	std::cout << "---------------------------------" << std::endl;
+}
+#pragma endregion
+
+#pragma region TestVariadicFunction
+bool IsDoubleDefault(const double& x)
+{
+	return (x == DBL_MAX || x == DBL_MIN) ? true : false;
+}
+std::vector<double> VariadicFunction(const size_t n, const double d...)
+{
+	std::vector<double> res;
+
+	va_list args;
+	va_start(args, d);
+	res.push_back(d);
+	for (size_t i = 1; i < n; i++)
+	{		
+		double x = va_arg(args, double);
+		res.push_back(x);
+	}
+	va_end(args);
+
+	return res;
+}
+void TestVariadicFunction()
+{
+	size_t num = 5;
+	std::vector<double> v = VariadicFunction(num, 1.0, 3.0, 9.0, 5.0, 7.0);
+	for (size_t i = 0; i < num; i++)
+	{
+		std::cout << v[i] << "\t";
+	}
+	std::cout << "\n";
+}
+#pragma endregion
+
+
 int main(int argc, char* argv[])
 {
 	//TestMPI(argc, argv);
-	TestTemplateFunctions();
+	//TestTemplateFunctions();
+	//TestHex2String();
+	//TestTypeInfo();
+	TestVariadicFunction();
 
 	getchar();
 
